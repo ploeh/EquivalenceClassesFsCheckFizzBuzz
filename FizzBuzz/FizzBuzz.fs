3c8c432ca850994e8b7c40e135022373ecfd5220
Mark Seemann <mark@ploeh.dk>
2014-12-02 11:59:23 +0000

    Added empty project.

---
+++ FizzBuzz/FizzBuzz.fs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2014 Mark Seemann.
+// Permission to reproduce or modify is granted for personal, educational use.
+// No warranty implied.
+
+namespace Ploeh.Samples
+
+module FizzBuzz =
+    ()
+
+module Tests =
+    ()
